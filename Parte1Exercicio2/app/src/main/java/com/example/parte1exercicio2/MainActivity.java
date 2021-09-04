b43b41a8b3dd4821e0fcc7f4cce0cadd16a451c4
Your Name <you@example.com>
2021-09-04 13:30:07 -0300

    feat: working p1ex2

--- Parte1Exercicio2/app/src/main/java/com/example/parte1exercicio2/MainActivity.java
+++ Parte1Exercicio2/app/src/main/java/com/example/parte1exercicio2/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.parte1exercicio2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+    }
+
+    public void send(View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        i.putExtra("data", editText.getText().toString());
+        startActivity(i);
     }
 }
--- Parte1Exercicio2/app/src/main/java/com/example/parte1exercicio2/MainActivity2.java
+++ Parte1Exercicio2/app/src/main/java/com/example/parte1exercicio2/MainActivity2.java
@@ -1,14 +1,23 @@
 package com.example.parte1exercicio2;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        textView = findViewById(R.id.textView2);
+
+        String data = getIntent().getStringExtra("data");
+        textView.setText(data);
     }
 }
