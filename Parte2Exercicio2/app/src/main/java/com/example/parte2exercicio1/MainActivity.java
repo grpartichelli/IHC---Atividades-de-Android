63b7fba1328d356a036a8105cfbc1cf2c0abe152
Your Name <you@example.com>
2021-09-04 16:47:20 -0300

    feat: finish p1ex3

--- Parte2Exercicio2/app/src/main/java/com/example/parte2exercicio1/MainActivity.java
+++ Parte2Exercicio2/app/src/main/java/com/example/parte2exercicio1/MainActivity.java
@@ -15,29 +15,45 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor light;
+    private Sensor pressure;
     TextView lightTextView;
+    TextView pressureTextView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lightTextView = findViewById(R.id.lightTextView);
+        pressureTextView = findViewById(R.id.pressureTextView);
+
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        pressure =  sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
         if(light != null) {
             sensorManager.registerListener(MainActivity.this, light, SensorManager.SENSOR_DELAY_NORMAL);
         }else {
             lightTextView.setText("Sensor de luz não existe.");
         }
+
+        if(pressure != null) {
+            sensorManager.registerListener(MainActivity.this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
+        }else {
+            pressureTextView.setText("Sensor giroscópico não existe.");
+        }
     }
 
 
     public void onSensorChanged(SensorEvent event) {
-        Sensor sensor = event.sensor;f
+        Sensor sensor = event.sensor;
         if(sensor.getType() == Sensor.TYPE_LIGHT)
         {
             lightTextView.setText("Intensidade de Luz: " + event.values[0]);
+        }
+
+        if(sensor.getType() == Sensor.TYPE_GYROSCOPE)
+        {
+            pressureTextView.setText("Giroscópio X: " + event.values[0] +  " Y: " + event.values[1] + " Z: " + event.values[2]);
         }
     }
 
